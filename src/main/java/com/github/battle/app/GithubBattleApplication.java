6ffc276ac76b5568bba662c62e9c8ae760e95532
Emanuel <emanueldemarao20@gmail.com>
2026-08-28 23:50:13 +0100

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/github/battle/app/GithubBattleApplication.java
@@ -0,0 +1,13 @@
+package com.github.battle.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/battle/app/GithubBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.battle.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
